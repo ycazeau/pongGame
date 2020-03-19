d432dee943ff064cd8835e2e84cb6bac8fea5cf6
ycazeau <ycazeau23@gmail.com>
2020-03-19 05:55:13 -0700

    Emulating a virual screen size, Increase the size of the text amd add the push library

--- main.lua
+++ main.lua
@@ -1,14 +1,41 @@
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH  = 432
+VIRTUAL_HEIGHT = 243
+
+push = require 'push'
+
+--[[
+    Runs when the game firsst start uo, only one; used to in itislize the game
+]]
 function love.load()
-    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT,{
-        fullscreen = false,
-        vsync = true,
-        resizable = false
-    })
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+   push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+       fullscreen = false,
+       vsync = true,
+       resizable = false
+   })
 end
 
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
+--[[
+    Called after update by Love, used to draw anything to the screen, updated or otherwise
+]]
 function love.draw()
-    love.graphics.printf("Hello World", 0, WINDOW_HEIGHT/2-6, WINDOW_WIDTH, 'center')
+    push:apply('start')
+
+    love.graphics.printf(
+        "Hello Pong!",           -- Text to render
+        0,                      -- Startting X (0 since we're going to center it based on width)
+        VIRTUAL_HEIGHT / 2 - 6,      -- Starting Y(halfway down the screen)
+        VIRTUAL_WIDTH,          -- Number of pixels to center within (the entire screen here)
+        'center')              -- Alignement mode, can be 'center', 'left', 'right'
+
+    push:apply('end')     
 end
