15a5a4ad344967ce6e7e58101ac0a2a78d84d2ae
ycazeau <ycazeau23@gmail.com>
2020-03-22 01:58:04 -0700

    Setup the new font, draw Rectangle, the ball and also the paddles

--- main.lua
+++ main.lua
@@ -1,20 +1,30 @@
+
+push = require 'push'
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
 VIRTUAL_WIDTH  = 432
 VIRTUAL_HEIGHT = 243
 
-push = require 'push'
+
 
 --[[
     Runs when the game firsst start uo, only one; used to in itislize the game
 ]]
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
-   push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+
+    --[[
+        Setting up the new font
+    ]]
+    smallFont = love.graphics.newFont('font.ttf', 10)
+    love.graphics.setFont(smallFont)
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
        fullscreen = false,
-       vsync = true,
-       resizable = false
+       resizable = false,
+       vsync = true       
    })
 end
 
@@ -30,10 +40,22 @@ end
 function love.draw()
     push:apply('start')
 
+    --[[ the grey color]]
+    love.graphics.clear(40/255, 45/255, 52/255, 255/255)
+
+    --[[Draw the rectangle or ball ]]
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH /2-2, VIRTUAL_HEIGHT /2-2, 5, 5)
+
+    --[[ Draw the paddles]]
+    love.graphics.rectangle('fill', 5, 20,5, 20)
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 40,5, 20)
+
+
     love.graphics.printf(
+        
         "Hello Pong!",           -- Text to render
         0,                      -- Startting X (0 since we're going to center it based on width)
-        VIRTUAL_HEIGHT / 2 - 6,      -- Starting Y(halfway down the screen)
+        20,      -- Starting Y(halfway down the screen)
         VIRTUAL_WIDTH,          -- Number of pixels to center within (the entire screen here)
         'center')              -- Alignement mode, can be 'center', 'left', 'right'
 
