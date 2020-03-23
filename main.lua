76d372f3c181f3667706ba72fe2dc4f531921398
ycazeau <ycazeau23@gmail.com>
2020-03-23 05:13:49 -0700

    Implement the serving player feature

--- main.lua
+++ main.lua
@@ -41,6 +41,9 @@ function love.load()
     player1Score = 0
     player2Score = 0
    
+    servingPlayer = math.random(2) == 1 and 1 or 2
+
+    
     --[[ Position of the paddles on Y axis (the can only move up and down)]]
     --[[ They are global so they can be detected by other functions and modules]]
    player1 = Paddle(10, 30, 5, 20)
@@ -49,6 +52,12 @@ function love.load()
    -- Place the ball in the middle on the screen
    ball = Ball(VIRTUAL_WIDTH / 2-2, VIRTUAL_HEIGHT / 2-2, 4, 4)
 
+    if servingPlayer == 1 then
+        ball.dx = 100
+    else
+        ball.dx = -100
+    end
+    
    gameState = 'start'
 
 end
@@ -57,36 +66,12 @@ end
 function love.update(dt)
     if gameState == 'play' then
 
-        if ball.x <= 0 then
-            player2Score = player2Score + 1
-            ball:reset()
-            gameState = 'start'
-        end
-
-        if ball.x >= VIRTUAL_WIDTH - 4 then
-            player1Score = player1Score + 1
-            ball:reset()
-            gameState = 'start'
-        end
-
-        if ball:collides(player1) then
-            -- Deflect the ball to the right
+        -- Detect ball collision  with paddles
+        if ball:collides(player1) then 
             ball.dx = -ball.dx * 1.03
             ball.x = player1.x + 5
-        end
 
-            if ball.dy < 0 then
-                ball.dy = -math.random(10, 150)
-                else
-                ball.dy = math.random(10, 150)
-            end
-        end
-
-        if ball:collides(player2) then
-            -- Deflect the ball to the left
-            ball.dx = -ball.dx * 1.03
-            ball.x = player2.x - 4
-
+            -- Keep velocity going in the same direction, but randomize it
             if ball.dy < 0 then
                 ball.dy = -math.random(10, 150)
             else
@@ -94,19 +79,49 @@ function love.update(dt)
             end
         end
 
+        if ball:collides(player2) then
+            ball.dx = -ball.dx * 1.03
+            ball.x = player2.x - 4
+
+            -- Keep velocity going in the same direction, but randomize it
+            if ball.dy < 0 then
+                ball.dy = -math.random(10, 150)
+            else
+                ball.dy = math.random(10, 150)
+            end
+        end
+
+        -- Detect upper and lower screen boundary collision and reverse it collide
         if ball.y <=  0 then
-            -- Deflect the ball down
             ball.y = 0
             ball.dy = -ball.dy         
         end
 
-        if ball.y >= VIRTUAL_HEIGHT - 4 then
-            -- Deflect the ball up
+        -- The ball size
+        if ball.y >= VIRTUAL_HEIGHT - 4 then 
             ball.y = VIRTUAL_HEIGHT - 4
             ball.dy = -ball.dy
         end
-     
-   
+    
+        -- if we reach the left or right edge of the screen
+        -- go back and start and update the score
+        if ball.x < 0 then
+            player2Score = player2Score + 1
+            ball:reset()
+            ball.dx = 100
+            servingPlayer = 1
+            gameState = 'serve'
+        end
+
+        if ball.x > VIRTUAL_WIDTH then
+            player1Score = player1Score + 1
+            ball:reset()
+            ball.dx = -100
+            servingPlayer = 2
+            gameState = 'serve'
+        end
+        --
+     end
     --[[ move the left paddle or the player 1]]
     if love.keyboard.isDown('right') then
         --[[ use math.max to block the paddle at the top of the screen]]
@@ -146,6 +161,8 @@ function love.keypressed(key)
 
     elseif key == 'enter' or key == 'return' then
         if gameState == 'start' then
+            gameState = 'serve'
+        elseif gameState == 'serve' then
             gameState = 'play'
         end
     end
@@ -160,6 +177,14 @@ function love.draw()
 
     --[[ Using the font to set the welcome text at the top of the screen]]
     love.graphics.setFont(smallFont)
+
+    if gameState == 'start' then
+        love.graphics.printf("Welcome to Pong!", 0, 20, VIRTUAL_WIDTH, 'center')
+        love.graphics.printf("Press Enter To Play!", 0, 32, VIRTUAL_WIDTH, 'center')
+    elseif gameState == 'serve' then
+        love.graphics.printf("Player " .. tostring(servingPlayer) .. " 's turn!",0, 20, VIRTUAL_WIDTH, 'center')
+        love.graphics.printf("Press Enter to Serve!",0, 32, VIRTUAL_WIDTH, 'center')
+    end
 
     love.graphics.setFont(scoreFont)
     
